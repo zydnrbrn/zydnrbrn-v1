516246a8ed76d0eda7ee233dfb9a21b40f2221b1
zydnrbrn <zidanreborn@gmail.com>
2022-10-08 14:40:00 +0700

    change using tailwindcss without cdn

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+    module.exports = {
+    darkMode: 'class',
+    content: ["./src/**/*.{html,js}","index.html"],
+  theme: {
+    fontFamily: {
+        'righteous': ['Righteous']
+    },
+    extend: {
+      colors: {
+        'main-red': '#BB3853'
+      }
+    }
+  }
+}
+
